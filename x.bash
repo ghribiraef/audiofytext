8885018d045611ad12cedac54d7d768e944d5131
ghribiraef <43656023+ghribiraef@users.noreply.github.com>
2024-12-29 22:32:18 -0500

    Delete x.bash

--- x.bash
+++
@@ -1,4 +0,0 @@
-curl -X POST http://localhost:3000/convert \
-     -H "Content-Type: application/json" \
-     -d '{"engine": "Google", "data": {"text": "Hello", "voice": "en-US"}}' \
-     -o response.json
